7f62f0555a0e642dc194c154987d153f2ee46c96
ashwanipanday2023-alt <ashwanipanday2023@gmail.com>
2026-01-12 23:59:07 +0530

    Add files via upload

---
+++ Day 12/first_last_position.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int> ans(2, -1);
+
+        int n = nums.size();
+        if (n == 0) return ans;
+
+        // Find first occurrence
+        int low = 0, high = n - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) {
+                ans[0] = mid;
+                high = mid - 1;   // move left
+            }
+            else if (nums[mid] < target) {
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+
+        // Find last occurrence
+        low = 0;
+        high = n - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) {
+                ans[1] = mid;
+                low = mid + 1;   // move right
+            }
+            else if (nums[mid] < target) {
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+};
